721a386cd1fc456e5b4dbba86df3b7f839eecb57
Mahmoud Ramadan <m_ramadn55@yahoo.com>
2018-02-14 21:50:18 +0200

    stor  (NOT WORKING)

--- Forums/models.py
+++ Forums/models.py
@@ -1,20 +1,9 @@
 
-
 class Member:
     def __init__(self, name, age):
         self.name = name
         self.age = age
         self.members = []
-
-class Member:
-	def __init__(self, name, age):
-		self.name = name
-		self.age = age
-
-class Member():
-	def __init__(self, name, age):
-		self.name = name
-		self.age = age
 
 
 class Post:
@@ -24,13 +13,7 @@ class Post:
         self.posts = []
 
 
-class Post():
-	def __init__(self, title, content):
-		self.title = title
-		self.content = content
 
-class Post():
-	def __init__(self, title, content):
-		self.title = title
-		self.content = content
+
+
 
